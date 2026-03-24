0fc14de543d19924b98d653c55619243a122ee51
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-03-24 12:05:30 +0530

    Understanding Path Parameter

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = FastAPI()
 def home():
     return {"message": "Hello, World!"}
 
+@app.get("/student/{student_id}")
+def get_student(student_id: int):
+    return {"student_id": student_id, "message": f"Fetching student {student_id}"}
+
