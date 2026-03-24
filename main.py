ed0b8777825f1168078eb7ef58f2118ed274b65c
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-03-24 12:06:42 +0530

    Understanding query parameter

--- main.py
+++ main.py
@@ -12,3 +12,32 @@ def home():
 def get_student(student_id: int):
     return {"student_id": student_id, "message": f"Fetching student {student_id}"}
 
+@app.get("/student/{student_id}/subject/{subject_name}")
+def get_subject(student_id: int, subject_name: str):
+    return {
+        "student_id": student_id,
+        "subject": subject_name
+    }
+
+"""@app.get("/students")
+def get_students(city: str, age: int):
+    return {
+        "city": city,
+        "age": age,
+        "message": f"Fetching students from {city} aged {age}"
+    }"""
+from typing import Optional
+
+@app.get("/students")
+def get_students(city: str, age: Optional[int] = None):
+    if age:
+        return {"city": city, "age": age}
+    return {"city": city, "age": "not provided"}
+
+@app.get("/student/{student_id}/results")
+def get_results(student_id: int, subject: Optional[str] = None):
+    if subject:
+        return {"student_id": student_id, "subject": subject}
+    return {"student_id": student_id, "subject": "all subjects"}
+
+
