ca8a10c92a706ac8153f9edf10ec2096bc197677
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-11 22:28:23 +0530

    Learning making end points, currently made a view end point to view patients data

--- learning_project/main.py
+++ learning_project/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
+
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+        return data
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"message": "Hello, World!"}
+    return {"message": "Patient management system API"}
 
 @app.get("/about")
 def about():
-    return {"message": "This is a learning project using FastAPI."}
+    return {"message": "A fully functional API to manage patient records."}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
