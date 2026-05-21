d95fe7ee30d5198881b2ddb5c2c296871d566204
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-21 22:49:51 +0530

    Added manual type hints and data validation

--- learning_project/why_pydantic.py
+++ learning_project/why_pydantic.py
@@ -2,3 +2,28 @@ def insert_patient_data(name, age):
     print(name, age)
     print("Inserted into database")
 
+# A junior developer might incorrectly pass age as a string:
+insert_patient_data("Nitish", "30")
+
+def insert_patient_data(name: str, age: int):
+    print(name, age)
+    print("Inserted into database")
+
+# This still runs without any errors despite the type hints!
+insert_patient_data("Nitish", "30")
+
+def insert_patient_data(name: str, age: int):
+    # 1. Type Validation
+    if type(name) == str and type(age) == int:
+        
+        # 2. Data Validation
+        if age < 0:
+            raise ValueError("Age can't be negative")
+        else:
+            print(name, age)
+            print("Inserted into database")
+            
+    else:
+        raise TypeError("Incorrect data type")
+
+insert_patient_data("Nitish", "30") # Now this properly throws an error
