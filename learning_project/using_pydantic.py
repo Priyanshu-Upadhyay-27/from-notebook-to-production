e36ec615fefbc4c16268a878fe7ef8e6ba64e2b3
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-22 23:21:42 +0530

    Learnt Field and Annotated for custom data validation, and we can set default value using field too, replacing Optional

--- learning_project/using_pydantic.py
+++ learning_project/using_pydantic.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 class validate_patient_data(BaseModel):
-    name: str
+    name: Annotated[str, Field(max_length=20, title="Name of the Patient", 
+                               description="Enter the name of the patient and it should be smaller then 20 characters.",
+                               examples = ["Nitish", "Priyanshuu"])]
     email: EmailStr # It can validate the format of email is correct or not.
     linkedin_url: AnyUrl # It can validate any URL.
-    age: int
+    age: int = Field(ge=0, lt = 100)
     weight: float 
-    married: bool
+    married: Annotated[bool, Field(default = False, description="Enter the marriage status of the partner", 
+                                   title = "Married or Not", examples= "True or False")]
     allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
 
@@ -22,7 +25,7 @@ def insert_patient_data(patient: validate_patient_data):
     print(f"Contact Details: {patient.contact_details}")
     print("Inserted")
 
-patient_info = {"name":"Priyanshu", "email": "abc@gmail.com", "age":"30", "weight": 80.3, "married":True,
+patient_info = {"name":"Priyanshuuuuu", "email": "abc@gmail.com", "age":"-30", "weight": 80.3, "married":True,
                  "allergies":["Pollen", "dogs", "bees", "dust"], "linkedin_url": "https://www.linkedin.com/in/priyanshu-upadhyay-cse/",
                  "contact_details":{"phone no.:":"9730735220"}} # here pydantic is converting, string 30 into integer 30.
 
