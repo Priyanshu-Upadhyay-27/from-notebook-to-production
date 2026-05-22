174071fadba396a4c216ce78c4d2cfbdf7dd4aa5
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-22 20:21:53 +0530

    Added another parameter called weight and learned automatic data conversion in pydantic

--- learning_project/using_pydantic.py
+++ learning_project/using_pydantic.py
@@ -3,13 +3,15 @@ from pydantic import BaseModel
 class validate_patient_data(BaseModel):
     name: str
     age: int
+    weight: float
 
 def insert_patient_data(patient: validate_patient_data):
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
     print("Inserted")
 
-patient_info = {"name":"Priyanshu", "age":30}
+patient_info = {"name":"Priyanshu", "age":"30", "weight": 68.9} # here pydantic is converting, string 30 into integer 30.
 
 patient1 = validate_patient_data(**patient_info)
 
