28afb438b282821c3dfbd5e4dd00a10b0243006c
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-22 20:13:09 +0530

    Created a simple pydantic class and instantiated it using the object

---
+++ learning_project/using_pydantic.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class validate_patient_data(BaseModel):
+    name: str
+    age: int
+
+def insert_patient_data(patient: validate_patient_data):
+    print(patient.name)
+    print(patient.age)
+    print("Inserted")
+
+patient_info = {"name":"Priyanshu", "age":30}
+
+patient1 = validate_patient_data(**patient_info)
