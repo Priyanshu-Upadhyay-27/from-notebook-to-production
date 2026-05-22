9833e9d1dc78141757834398b4808505a0c22c2a
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-22 22:57:44 +0530

    Learnt about custom classes, which can help in data validation like email or URL

--- learning_project/using_pydantic.py
+++ learning_project/using_pydantic.py
@@ -1,16 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 class validate_patient_data(BaseModel):
     name: str
+    email: EmailStr # It can validate the format of email is correct or not.
+    linkedin_url: AnyUrl # It can validate any URL.
     age: int
-    weight: float
-    married: bool = False
+    weight: float 
+    married: bool
     allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
 
 
 def insert_patient_data(patient: validate_patient_data):
     print(f"Patient Name: {patient.name}")
+    print(f"Patients Contact info: {patient.email}")
+    print(f"Patient's LinkedIn: {patient.linkedin_url}")
     print(f"patient Age: {patient.age}")
     print(f"Weight of the patient: {patient.weight}")
     print(f"Is the Patient Married: {patient.married}")
@@ -18,8 +22,9 @@ def insert_patient_data(patient: validate_patient_data):
     print(f"Contact Details: {patient.contact_details}")
     print("Inserted")
 
-patient_info = {"name":"Priyanshu", "age":"30", "weight": 80.3, 
-            "contact_details":{"email":"xyz@gmail.com", "phone no.:":"9730735220"}} # here pydantic is converting, string 30 into integer 30.
+patient_info = {"name":"Priyanshu", "email": "abc@gmail.com", "age":"30", "weight": 80.3, "married":True,
+                 "allergies":["Pollen", "dogs", "bees", "dust"], "linkedin_url": "https://www.linkedin.com/in/priyanshu-upadhyay-cse/",
+                 "contact_details":{"phone no.:":"9730735220"}} # here pydantic is converting, string 30 into integer 30.
 
 patient1 = validate_patient_data(**patient_info)
 
