b2190a7a90331c6711db7630d860a3c54a8ec3d1
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-22 20:33:28 +0530

    Added more complex fields like list, dictionary and performed data type validation using Pydantic, and also checked when unintended value comes

--- learning_project/using_pydantic.py
+++ learning_project/using_pydantic.py
@@ -1,17 +1,25 @@
 from pydantic import BaseModel
-
+from typing import List, Dict
 class validate_patient_data(BaseModel):
     name: str
     age: int
     weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
 
 def insert_patient_data(patient: validate_patient_data):
-    print(patient.name)
-    print(patient.age)
-    print(patient.weight)
+    print(f"Patient Name: {patient.name}")
+    print(f"patient Age: {patient.age}")
+    print(f"Weight of the patient: {patient.weight}")
+    print(f"Is the Patient Married: {patient.married}")
+    print(f"Allergies in the patient:{patient.allergies}")
+    print(f"Contact Details: {patient.contact_details}")
     print("Inserted")
 
-patient_info = {"name":"Priyanshu", "age":"30", "weight": 68.9} # here pydantic is converting, string 30 into integer 30.
+patient_info = {"name":"Priyanshu", "age":"30", "weight": 80.3, "married":True,
+                 "allergies":["Pollen", "dogs", "bees", "dust"], "contact_details":{"email":"xyz@gmail.com", "phone no.:":"9730735220"}} # here pydantic is converting, string 30 into integer 30.
 
 patient1 = validate_patient_data(**patient_info)
 
