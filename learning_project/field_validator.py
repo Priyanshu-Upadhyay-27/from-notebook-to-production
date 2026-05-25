af18ce115714296c269158d1e5fb945879a10abe
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-25 18:09:16 +0530

    Field validator works well with valid domains and returning the value from the function is significant, else it will return None for the same field

--- learning_project/field_validator.py
+++ learning_project/field_validator.py
@@ -3,7 +3,7 @@ from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
     name: str
-    email:EmailStr
+    email: EmailStr
     age:int
     weight:float
     married:bool
@@ -19,6 +19,7 @@ class Patient(BaseModel):
             print(f"Person recognized from {domain_name}")
         else:
             raise ValueError("Not a valid domain")
+        return value # retun value is very significant., as the email will be None, if no return is there. 
         
 def insert_patient_data(patient: Patient):
     print(f"Patient Name: {patient.name}")
@@ -27,7 +28,7 @@ def insert_patient_data(patient: Patient):
     print(f"Weight of the patient: {patient.weight}")
     print(f"Is the Patient Married: {patient.married}")
 
-patient_info = {"name":"Priyanshuuuuu", "email": "abc@gmail.com", "age":"30", "weight": 80.3, "married":True,
+patient_info = {"name":"Priyanshuuuuu", "email": "abc@icici.com", "age":"30", "weight": 80.3, "married":True,
                  "allergies":["Pollen", "dogs", "bees", "dust"], "contact_details":{"phone no.:":"9730735220"}}
 
 patient1 = Patient(**patient_info)
