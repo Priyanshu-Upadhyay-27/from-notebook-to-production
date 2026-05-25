bef612bdc111a0d94595bfb51e25d925c6f7b11b
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-26 01:26:59 +0530

    Built a pydantic class with well documented and proper conditions

--- learning_project/post_req.py
+++ learning_project/post_req.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI, Path, Query, HTTPException
+from fastapi import FastAPI, Path, Query, HTTPException, Field
+from pydantic import BaseModel
+from typing import Annotated, Literal
 import json
+
+class Patient(BaseModel):
+    id: Annotated[str, Field(..., description = "Id of the Patient", examples = ["P001"])]
+    name: Annotated[str, Field(..., description = "Name of the Patient", examples = ["Ankit"])]
+    city: Annotated[str, Field(..., description = "City of the Patient", examples = ["Agra", "Delhi"])]
+    age: Annotated[int, Field(..., gt = 0, lt = 110, description = "Age of the Patient", examples = [27, 68])]
+    gender: Annotated[Literal["male", "female", "others"], Field(..., description = "Gender of the patient", examples = ["male", "female", "others"])]
+    height: Annotated[float, Field(..., gt = 0, description = "Height of the patient(in meter)", examples = [1.72, 1.67] )]
+    weight: Annotated[float, Field(..., gt = 0, description = "Weight pf the patient(in Kg)", examples = [75, 68])]
+
+
 
 app1 = FastAPI()
 
