ff8fed45f424f607088a690b99d16cec05a7681c
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-05-27 02:00:09 +0530

    Added delete endpoint for deleting the patient details from the db

--- learning_project/post_req.py
+++ learning_project/post_req.py
@@ -122,7 +122,7 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
     existing_patient_info = data[patient_id]
 
     updated_patient_info = patient_update.model_dump(exclude_unset = True) 
-    # serialization: converts to dictionary and it will not inlcude None values
+    # serialization: converts to dictionary and exclude_unset will not inlcude None values
 
     for key, value in updated_patient_info.items():
         existing_patient_info[key] = value
@@ -135,3 +135,16 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
 
     save_data(data)
     return JSONResponse(status_code=200, content={"message":"Patient details updated"})
+
+@app1.delete("/delete/{patient_id}")
+def delete_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message": "Patient details deleted"})
